cd174b01c98fbc6487f47081d84b90195b82b6eb
alestor123 <alestoraldous@gmail.com>
2021-01-13 16:13:28 +0530

    removed err

--- cli.js
+++ cli.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 var ss = require('./App'),
-options = require('minimist')(process.argv.slice(2));
+options = require('minimist')(process.argv.slice(2)),
+chalk = require('chalk');
+(async () => {
+var data = await ss(options.s || options.search)
+data.map(item,index => console.log(item))
+})();
