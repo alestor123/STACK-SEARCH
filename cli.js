83bca515ba0ad808aa581b3b250733e8a9e98d67
alestor123 <alestoraldous@gmail.com>
2021-01-13 16:25:19 +0530

    added cli code

--- cli.js
+++ cli.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 var ss = require('./App'),
 options = require('minimist')(process.argv.slice(2)),
+searchKey = options.s || options.search,
 chalk = require('chalk');
+if(!searchKey){ 
+console.log(chalk.red('Search Key Not Found')) 
+process.exit(0)}
 (async () => {
-var data = await ss(options.s || options.search)
-data.map(item,index => console.log(item))
+var data = await ss(searchKey)
+data.forEach((item,index) => console.log((data.length-index)+" "+chalk.bold.yellow(item.title)+"\n"+chalk.blue(item.link)+'\n\n'))
 })();
